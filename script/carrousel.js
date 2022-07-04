b2b77ec7330e8cc91bb9e82d3a86122fd7766d62
Hanane05 <e2195837@cmaisonneuve.qc.ca>
2022-07-04 00:41:42 -0400

    s7-c13- Ajouter du code dans carrousel.js pour bien targeter les images du carrousel

---
+++ script/carrousel.js
@@ -0,0 +1,14 @@
+(function(){
+    console.log("début du carrousel");
+    // let elmGalerie = document.querySelector(".galerie")
+    // console.log(elmGalerie);
+
+    let elmGalerie__img = document.querySelectorAll('.galerie figure img');
+    console.log(elmGalerie__img);
+
+    for(const img of elmGalerie__img){
+        img.addEventListener('mousedown', function () {
+            console.log(img.getAttribute('src'));
+        })
+    }
+})()
