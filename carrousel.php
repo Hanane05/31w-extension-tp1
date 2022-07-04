1cdb5380e1cf86893c31b7420eac58be2eeaa9bf
Hanane05 <e2195837@cmaisonneuve.qc.ca>
2022-07-03 23:31:54 -0400

    s7-c13- Début de la création d'une extension carrousel et ajouter de code dans carrousel.php en s'inspirant du fichier short_2.php

---
+++ carrousel.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Plugin Name: Caroussel
+ * Author: Hanane Boukhalfa
+ * Description: Carrousel d'image créé à partir d'une galerie
+ * Plugin URI: https://github.com/Hanane05/31w-extension-tp1
+ */
+
+function carrousel_31w_enqueue()
+{
+    $version_css = filemtime(plugin_dir_path(__FILE__) ."style.css");
+    $version_js =  filemtime(plugin_dir_path(__FILE__) ."script/carrousel.js");
+    // var_dump(__FILE__); die();
+
+    wp_enqueue_style('carrousel_31w_css',
+                     plugin_dir_url(__FILE__) . "style.css", 
+                     array(), 
+                     $version_css);
+
+    wp_enqueue_script('carrousel_31w_js',
+                    plugin_dir_url(__FILE__) . "script/carrousel.js",
+                    array(), 
+                    $version_js,
+                    true);
+}
+add_action('wp_enqueue_scripts', "carrousel_31w_enqueue");
+
+
+
+function genere_carrousel(){
+    ///////////////////////////////HTML
+    $contenu = '<div class="carrousel"';
+    $contenu .= '<button class="carrousel__fermeture">X</button>';
+    $contenu .= '<figure class="carrousel__img"></figure>';
+    $contenu .= '<form class="carrousel__radio"></form>';
+    $contenu .= '</div>';
+    $contenu .= '<button id="mon_bouton">OK</button>';
+    return $contenu;
+
+}
+
+add_shortcode('carrousel', 'genere_carrousel');
